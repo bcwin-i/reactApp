9ff172374139e3d4bf21d6397b93815db225c0f4
bcwin-i <iziqbek@gmail.com>
2021-08-13 03:54:53 +0000

    Updates

---
+++ precache-manifest.ac2daef9276fc461a28b8c128eb02b18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597947a7a183f33e67ae16f0da89cea1",
+    "url": "/bcwin-i/reactApp/index.html"
+  },
+  {
+    "revision": "ac972f52633d985cb51a",
+    "url": "/bcwin-i/reactApp/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "0a6192b696cd04b81428",
+    "url": "/bcwin-i/reactApp/static/css/main.9726b4d9.chunk.css"
+  },
+  {
+    "revision": "ac972f52633d985cb51a",
+    "url": "/bcwin-i/reactApp/static/js/2.88889c52.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/bcwin-i/reactApp/static/js/2.88889c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6192b696cd04b81428",
+    "url": "/bcwin-i/reactApp/static/js/main.385e17fe.chunk.js"
+  },
+  {
+    "revision": "72a90a70e80497a737de",
+    "url": "/bcwin-i/reactApp/static/js/runtime-main.0315dcdd.js"
+  },
+  {
+    "revision": "acc68e52cc0106d46c7f1d0c8a69be34",
+    "url": "/bcwin-i/reactApp/static/media/ultraviolentbb_reg.acc68e52.woff"
+  }
+]);
