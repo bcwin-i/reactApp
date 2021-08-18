e5d91f4f77849b0896fc3d4a8cf5cd7706fd0e8d
bcwin-i <iziqbek@gmail.com>
2021-08-18 03:58:57 +0000

    Hooks update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import {robots} from '../robots.js';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: robots,
-      searchfield: ''
-    }
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       robots: robots,
+//       searchfield: ''
+//     }
+//   }
+
+//   componentDidMount() {
+//     // fetch('https://jsonplaceholder.typicode.com/users')
+//     //   .then(response => response.json())
+//     //   .then(users => {this.setState({ robots: users})});
+//   }
+
+//   onSearchChange = (event) => {
+//     this.setState({ searchfield: event.target.value })
+//   }
+
+//   render() {
+//     const { robots, searchfield } = this.state;
+//     const filteredRobots = robots.filter(robot =>{
+//       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+//     })
+//     return !robots.length ?
+//       <h1>Loading</h1> :
+//       (
+//         <div className='tc'>
+//           <h1 className='title'>
+//             RoboFriends
+//             <SearchBox searchChange={this.onSearchChange}/>
+//           </h1>
+//           <Scroll>
+//             <CardList robots={filteredRobots} />
+//           </Scroll>
+//         </div>
+//       );
+//   }
+// }
+
+// export default App;
+
+function App(){
+  const [bots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
+  const [count, setCount] = useState(0); 
+
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {setRobots(users)});
+      console.log(count);
+  }, [count]); //only run when count changes
+
+  const onSearchChange = (event) => {
+    setSearchField(event.target.value)
   }
 
-  componentDidMount() {
-    // fetch('https://jsonplaceholder.typicode.com/users')
-    //   .then(response => response.json())
-    //   .then(users => {this.setState({ robots: users})});
-  }
+  const filteredRobots = bots.filter(robot =>{
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  })
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
+  //console.log(bots, searchField);
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !robots.length ?
-      <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='title'>
-            RoboFriends
-            <SearchBox searchChange={this.onSearchChange}/>
-          </h1>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-  }
+  return !robots.length ?
+    <h1 className='title'>Loading</h1> :
+    (
+      <div className='tc'>
+        <h1 className='title'>
+          RoboFriends
+          {/* <button onClick={() => setCount(count+1)}>Click me!{count}</button> */}
+          <SearchBox searchChange={onSearchChange}/>
+        </h1>
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+          
 }
 
 export default App;
